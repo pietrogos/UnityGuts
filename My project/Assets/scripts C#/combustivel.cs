22f3e975f1bbd6d351e609a6436a550f9da8424d
guilherme.barreto <g.goulart97@edu.pucrs.br>
2023-05-20 17:37:30 -0300

    degradacao fogueira e adicao layout local

--- My project/Assets/scripts C#/combustivel.cs
+++ My project/Assets/scripts C#/combustivel.cs
@@ -8,6 +8,36 @@ public class combustivel : MonoBehaviour
     public Color blueColor;
 
     public int fuel = 0;
+    private float update;
+
+    
+    private void Update()
+    {
+        update += Time.deltaTime;
+
+        ParticleSystem.MainModule main = GetComponent<ParticleSystem>().main;
+        ParticleSystem.EmissionModule emission = GetComponent<ParticleSystem>().emission;
+        Debug.Log(main.startLifetime.constant);
+        Debug.Log(emission.rateOverTime.constant);
+
+        float currentLifetime = main.startLifetime.constant;
+        float currentRate =  emission.rateOverTime.constant;
+
+        if (update > 5.0f)
+        {
+            update = 0.0f;
+
+            if (currentLifetime - 0.01f >= 0.5f) 
+            {
+                main.startLifetime = currentLifetime - 0.01f;
+            }
+
+            if (currentRate - 2f >= 86.59)
+            {
+                emission.rateOverTime = currentRate - 2f;
+            }
+        }
+    }
 
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
